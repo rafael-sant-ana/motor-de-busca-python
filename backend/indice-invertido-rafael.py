e8545f26d9f9b6749bf832694d2b6c0fa4192bdb
Rafael Sant'Ana <rafael.santana@dcc.ufmg.br>
2025-10-17 11:26:47 -0300

    indice invertido basico (rafael&mateus)

--- backend/indice-invertido-rafael.py
+++ backend/indice-invertido-rafael.py
@@ -1,8 +1,12 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self, value, position=None):
         self.children = {}
         self.value = value
         self.is_end = True
+        self.positions = [position] if position is not None else []
+
+    def append_occourence(self, position):
+        self.positions.append(position)
 
     def add_child(self, node):
         if len(node.value) == 0:
@@ -37,7 +41,7 @@ class TrieCompacta:
 
         return i
 
-    def insert_rec(self, root, value):
+    def insert_rec(self, root, value, position):
         if len(value) == 0:
             return
         # consome o maximo da palavra
@@ -52,13 +56,15 @@ class TrieCompacta:
             child = Node(rest_root)
             child.children = root.children
             child.is_end = root.is_end
+            child.positions = root.positions
 
             root.value = root.value[:i]
             root.is_end = False
             root.children = {child.value[0]: child}
+            root.positions = []
 
             if rest_value:  # exemplo, inserindo chae em chave, precisamos colocar "e" como sendo fim, e "ve" tambem
-                root.add_child(Node(rest_value))
+                root.add_child(Node(rest_value, position))
 
             else:  # exemplo, inserindo cha em chave
                 # nosso root seria cha
@@ -67,23 +73,23 @@ class TrieCompacta:
         else:  # i >= lenroot val quer dizer, inserindo "chavea" e, "chave"
             if not rest_value:  # inserindo chave em chave
                 root.is_end = True
+                root.append_occourence(position)
             else:  # ainda temos um a pra inserir em algum filho
                 next_child = root.retrieve_child(rest_value)
-
                 if next_child:  # inserir "chaveabe" depois de ter "chave" e "chavea"
-                    self.insert_rec(next_child, value)
+                    self.insert_rec(next_child, value, position)
                 else:  # inserir "chavea" e n tem nada com o prefixo "a" depois de "chave"
-                    root.add_child(Node(rest_value))
+                    root.add_child(Node(rest_value, position))
 
-    def insert(self, value):
-        self.insert_rec(self.head, value)
+    def insert(self, value, position):
+        self.insert_rec(self.head, value, position)
 
     def find(self, word):
         word_tot_length = len(word)
         consumed_so_far = 0
         root = self.head
 
-        while root != None:
+        while root is not None:
             i = self.consume_word(root.value, word)
             consumed_so_far = consumed_so_far + i
 
@@ -108,16 +114,42 @@ class TrieCompacta:
 
         for _, child in sorted(node.children.items()):
             end_marker = " (End) " if child.is_end else ""
-            print(f"{indent}└── '{child.value}'{end_marker}")
+            print(
+                f"{indent}└── '{child.value}'{end_marker} ({','.join(str(x) for x in child.positions)})"
+            )
             self.log_tree(child, indent + "    ")
+
+    # quero gerar todas as palavras de volta. pensei numa DFS para ir construindo palavra-a-palavra.
+    def construct_index(self):
+        root = self.head
+
+        words = []
+
+        def dfs(root, prefix):
+            if root is None:
+                words.append(prefix)
+                return
+
+            prefix_appended = prefix + root.value
+
+            if root.is_end:
+                words.append(prefix_appended)
+
+            for _, child in sorted(root.children.items()):
+                dfs(child, prefix_appended)
+
+        dfs(root, "")
+
+        return "\n".join(str(x) for x in words)
 
 
 if __name__ == "__main__":
+    text = """Lorem ipsum dolor sit ameteuismod. Suspendisse eu velit tortor. Morbi vehicula neque ut mi auctor, ut posuere turpis finibus."""
+
     trie = TrieCompacta()
 
-    trie.insert("chave")
-    trie.insert("cha")
+    for idx, word in enumerate(text.split(" ")):
+        # print(word)
+        trie.insert(word, idx)
 
-    print(trie.find("cha"))
-
-    trie.log_tree()
+    print(trie.log_tree())
