1883afd7166bdba8c234d58d508a4a2150353dc2
mJesu <mateusjesueuwork@gmail.com>
2025-10-20 23:20:31 -0300

    minor change

--- testes-de-implementacao/indice-invertido-rafael.py
+++ testes-de-implementacao/indice-invertido-rafael.py
@@ -148,9 +148,7 @@ class TrieCompacta:
 
 
 if __name__ == "__main__":
-    # text = """Lorem ipsum dolor sit ameteuismod. Suspendisse eu velit tortor. Morbi vehicula neque ut mi auctor, ut posuere turpis finibus."""
-
-    text = """Segundo outros, finalmente, a alma é um ser moral, distinto, independente da matéria e que conserva sua individualidade após a morte."""
+    text = """Lorem ipsum dolor sit ameteuismod. Suspendisse eu velit tortor. Morbi vehicula neque ut mi auctor, ut posuere turpis finibus."""
 
     # Processamento do texto para remover pontuações e converter tudo para minúsculas
     text = text.translate(str.maketrans("", "", string.punctuation)).lower()
